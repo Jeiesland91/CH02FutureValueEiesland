9237cf3d7ca8ceeca7326a7c6719fc6dc34e5647
Mr. Jay <jdeiesland@dmacc.edu>
2021-02-01 19:32:10 -0600

    Add project files.

---
+++ Models/FutureValueModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace Ch02FutureValueEiesland.Models
+{
+    public class FutureValueModel
+    {
+        [Required]
+        [Range(1,500)]
+        public decimal? MonthlyInvestment { get; set; }
+        [Required]
+        public decimal? YearlyInterestRate { get; set; }
+
+        public int Years { get; set; }
+
+        public decimal CalculateFutureValue()
+        {
+            int months = Years * 12;
+            decimal monthlyInterestRate = YearlyInterestRate.Value / 12 / 100;
+            decimal futureValue = 0;
+            for (int i = 0; i < months; i++)
+            {
+                futureValue = (futureValue + MonthlyInvestment.Value) *
+                              (1 + monthlyInterestRate);
+
+            }
+            return futureValue;
+        }
+    }
+}
